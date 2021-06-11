1283b29e57c99b74e9f0947286b289926f61e82a
Michael Angelo De Leon <michael19972910@gmail.com>
2021-06-11 11:46:00 +0800

    remove gad_plans_id  input

--- resources/views/admin/proposal/components/form-elements.blade.php
+++ resources/views/admin/proposal/components/form-elements.blade.php
@@ -1,11 +1,3 @@
-<div class="form-group row align-items-center" :class="{'has-danger': errors.has('gad_plans_id'), 'has-success': fields.gad_plans_id && fields.gad_plans_id.valid }">
-    <label for="gad_plans_id" class="col-form-label text-md-right" :class="isFormLocalized ? 'col-md-4' : 'col-md-2'">{{ trans('admin.proposal.columns.gad_plans_id') }}</label>
-        <div :class="isFormLocalized ? 'col-md-4' : 'col-md-9 col-xl-8'">
-        <input type="text" v-model="form.gad_plans_id" v-validate="'required'" @input="validate($event)" class="form-control" :class="{'form-control-danger': errors.has('gad_plans_id'), 'form-control-success': fields.gad_plans_id && fields.gad_plans_id.valid}" id="gad_plans_id" name="gad_plans_id" placeholder="{{ trans('admin.proposal.columns.gad_plans_id') }}">
-        <div v-if="errors.has('gad_plans_id')" class="form-control-feedback form-text" v-cloak>@{{ errors.first('gad_plans_id') }}</div>
-    </div>
-</div>
-
 <div class="form-group row align-items-center" :class="{'has-danger': errors.has('letter_body'), 'has-success': fields.letter_body && fields.letter_body.valid }">
     <label for="letter_body" class="col-form-label text-md-right" :class="isFormLocalized ? 'col-md-4' : 'col-md-2'">{{ trans('admin.proposal.columns.letter_body') }}</label>
     <div :class="isFormLocalized ? 'col-md-4' : 'col-md-9 col-xl-8'">
