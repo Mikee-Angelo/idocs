bf4cdaf66862a69832bae8f57c5257f72b6920ce
Michael Angelo De Leon <michael19972910@gmail.com>
2021-06-22 06:58:48 +0800

    added Accomplishment module

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -10,8 +10,8 @@
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/proposals') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.proposal.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/liquidations') }}"><i class="nav-icon icon-flag"></i> {{ trans('admin.liquidation.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/reimbursements') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.reimbursement.title') }}</a></li>
+               <li class="nav-item"><a class="nav-link" href="{{ url('admin/accomplishments') }}"><i class="nav-icon icon-drop"></i> {{ trans('admin.accomplishment.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/calendar') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.calendar.title') }}</a></li>
-         
             
             @if(Auth::user()->roles()->pluck('id')[0] == 1)
                 <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.others') }}</li>
@@ -22,7 +22,8 @@
                 <li class="nav-item"><a class="nav-link" href="{{ url('admin/units') }}"><i class="nav-icon icon-puzzle"></i> {{ trans('admin.unit.title') }}</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('admin/event-types') }}"><i class="nav-icon icon-graduation"></i> {{ trans('admin.event-type.title') }}</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('admin/announcements') }}"><i class="nav-icon icon-flag"></i> {{ trans('admin.announcement.title') }}</a></li>
-   {{-- Do not delete me :) I'm used for auto-generation menu items --}}
+
+           {{-- Do not delete me :) I'm used for auto-generation menu items --}}
        
                 <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.settings') }}</li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('admin/admin-users') }}"><i class="nav-icon icon-user"></i> {{ __('Manage access') }}</a></li>
