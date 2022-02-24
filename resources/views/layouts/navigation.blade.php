1d923de7727288cd11f58e6bf5fd5537c90fbfce
Michael Angelo De Leon <iammikedev@gmail.com>
2022-02-25 01:10:48 +0800

    implemented gad plan table

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,13 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                 <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('gadplans.index')" :active="request()->routeIs('gadplans.index')">
+                        {{ __('GAD') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
