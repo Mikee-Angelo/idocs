c4ea6c49100408e4d8c3618b4ad029d9adde7dbf
Michael Angelo De Leon <iammikedev@gmail.com>
2022-03-06 23:07:02 +0800

    fixed create button

--- resources/views/campus/index.blade.php
+++ resources/views/campus/index.blade.php
@@ -8,10 +8,11 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-             <div class="flex flex-row-reverse mb-3">
-                <x-link href="{{route('campus.create')}}" class="ml-3 w-32">
-                    {{ __('Compose') }}
-                </x-link>
+            <div class="flex flex-row-reverse mb-3">
+
+                <a href="{{route('campus.create')}}"
+                    class="justify-end inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Compose</a>
+
             </div>
 
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -22,5 +23,3 @@
         </div>
     </div>
 </x-app-layout>
-
-
